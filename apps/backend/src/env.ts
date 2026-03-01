278ecb33fb9454e288afe9c2548f1029f44d461a
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-03-01 03:37:34 +0200

    test ci

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -18,3 +18,5 @@ const envSchema = z.object({
 export const env = envSchema.parse(process.env)
 export const isDev = env.NODE_ENV === NodeEnv.DEV
 export const isProd = env.NODE_ENV === NodeEnv.PRODUCTION
+
+//test dfasdf
