47cdea981cdf6192d98ca0d4b928694557419805
pika <pikazor64@gmail.com>
2026-03-03 00:05:09 +0300

    fix: remove rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,6 @@ export default [
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'unused-imports/no-unused-imports': 'error',
-      'prettier/prettier': 'warn',
     },
   },
 
