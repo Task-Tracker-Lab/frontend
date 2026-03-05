a58364b81ae02b291339bfbbf54ec875e4d6c31c
Pika <pikazor64@gmail.com>
2026-03-05 18:34:42 +0300

    feat: add husky, setup eslint, prettier (#7)
    * style: format code with prettier and eslint
    
    * style: format code with prettier and eslint
    
    * style: format code with prettier and eslint
    
    * style: format code with prettier and eslint
    
    * style: format code with prettier and eslint
    
    * style: format code with prettier and eslint
    
    * feat: setup eslint, prettier, husky and lint-staged
    
    * style: fix formatting
    
    * fix: changed scripts
    
    * feat: add scripts check
    
    * fix: delete removed unnecessary rules
    
    * fix: changed scripts
    
    * feat: add script
    
    * fix: format package.json
    
    * feat: add ignore files
    
    * fix: fix lint-staged
    
    * fix: remove rules
    
    * fix: fix file
    
    * fix: resolved package.json conflicts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,40 @@
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import unusedImports from 'eslint-plugin-unused-imports';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals'; // Добавили для работы окружений
 
-export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended);
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      globals: {
+        ...globals.es2021,
+      },
+    },
+  },
+  {
+    plugins: {
+      import: importPlugin,
+      'unused-imports': unusedImports,
+      prettier,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+
+  {
+    ignores: ['node_modules', 'dist', '.next', 'build', '**/jest.config.ts', '**/*.config.ts'],
+  },
+];
