8cd32d03c57b3f2380aefd531167b4f148e2e35f
pika <pikazor64@gmail.com>
2026-03-16 13:37:29 +0300

    feat:add login and register pages

---
+++ apps/frontend/app/auth/register/page.tsx
@@ -0,0 +1,8 @@
+import RegisterForm from "../../features/auth/register/ui/RegisterForm"
+export default function RegisterPage({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+        <RegisterForm />
+    </div>
+  );
+}
