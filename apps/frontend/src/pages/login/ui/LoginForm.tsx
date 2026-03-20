24742ac6012356c9fdaf713e20d776b9cd07df39
Pika <pikazor64@gmail.com>
2026-03-20 14:33:42 +0300

    feat: add login,register page (#17)
    Co-authored-by: kapitulin24 <73795694+kapitulin24@users.noreply.github.com>

---
+++ apps/frontend/src/pages/login/ui/LoginForm.tsx
@@ -0,0 +1,55 @@
+'use client';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { formSchema, FormState } from '../model/loginSchema';
+import { Field, FieldDescription, FieldLabel, Button, Input } from 'shared/ui';
+
+export function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(formSchema),
+    mode: 'onChange',
+  });
+  const onSubmit = (data: FormState): void => {
+    console.log(data);
+    reset();
+  };
+  return (
+    <form className="space-y-6 mb-4" onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col space-y-4">
+        <Field>
+          <FieldLabel htmlFor="email">Почта</FieldLabel>
+          <Input type="email" {...register('email')} id="email" placeholder="example@company.com" />
+          {errors.email && (
+            <FieldDescription className="text-red-500">{errors.email.message}</FieldDescription>
+          )}
+        </Field>
+        <Field className="grid gap-2">
+          <div className="flex items-center">
+            <FieldLabel htmlFor="password">Пароль</FieldLabel>
+            <Link
+              href="/auth/reset-password"
+              className="ml-auto text-sm underline-offset-4 hover:underline text-muted-foreground"
+            >
+              Забыли пароль?
+            </Link>
+          </div>
+
+          <Input id="password" type="password" placeholder="********" {...register('password')} />
+
+          {errors.password && (
+            <FieldDescription className="text-red-500 text-sm">
+              {errors.password.message}
+            </FieldDescription>
+          )}
+        </Field>
+      </div>
+      <Button className="w-full bg-blue-600 text-white py-2 rounded">Войти</Button>
+    </form>
+  );
+}
