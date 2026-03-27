f9d5296450470325489a122ee19f5ed62216151d
kapitulin24 <73795694+kapitulin24@users.noreply.github.com>
2026-03-27 13:39:23 +0300

    feat(TAS-1): add storybook (#19)

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -1,3 +1,4 @@
+import storybook from 'eslint-plugin-storybook';
 import baseConfig from '../../eslint.config.mjs';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
@@ -9,6 +10,7 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   ...pluginQuery.configs['flat/recommended'],
+  ...storybook.configs['flat/recommended'],
   globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
