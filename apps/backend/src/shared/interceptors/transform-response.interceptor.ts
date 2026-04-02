02adaa418bacd5a25c3c8660754a002ad6ac243c
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-04-02 11:37:33 +0300

    feat: update TransformResponseInterceptor
    - Add handling 204 status code
    - Change handling result if result is null

--- apps/backend/src/shared/interceptors/transform-response.interceptor.ts
+++ apps/backend/src/shared/interceptors/transform-response.interceptor.ts
@@ -1,14 +1,21 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 import { ApiResponse } from '../dto';
+import { FastifyReply } from 'fastify';
 
 @Injectable()
 export class TransformResponseInterceptor<T> implements NestInterceptor<T, ApiResponse<T>> {
   intercept(context: ExecutionContext, next: CallHandler): Observable<ApiResponse<T>> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse<FastifyReply>();
+
     return next.handle().pipe(
       map((result) => {
+        if (response.statusCode === 204) {
+          return undefined;
+        }
         if (result == null) {
-          return { data: null };
+          return {};
         }
         if (result.data) {
           return result;
