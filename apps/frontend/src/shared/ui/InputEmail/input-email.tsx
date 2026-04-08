2744aa5317646619d4fb8319869be8ce016a177b
kapitulin24 <kapitulin24@gmail.com>
2026-04-08 19:59:35 +0300

    fix(TAS-27): Исправлен тип инпута почты на text

--- apps/frontend/src/shared/ui/InputEmail/input-email.tsx
+++ apps/frontend/src/shared/ui/InputEmail/input-email.tsx
@@ -2,7 +2,15 @@ import * as React from 'react';
 import { Input } from 'shared/ui';
 
 function InputEmail(props: React.ComponentProps<typeof Input>) {
-  return <Input aria-label="Email" placeholder="mail@example.com" {...props} type="email" />;
+  return (
+    <Input
+      aria-label="Email"
+      placeholder="mail@example.com"
+      {...props}
+      type="text"
+      autoComplete="email"
+    />
+  );
 }
 
 export { InputEmail };
