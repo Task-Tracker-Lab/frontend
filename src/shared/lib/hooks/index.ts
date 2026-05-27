9ce74e8401e5ac2ffc8394aa14db2fa563e9d4ed
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2026-05-27 23:03:59 +0300

    fix: delete import

--- src/shared/lib/hooks/index.ts
+++ src/shared/lib/hooks/index.ts
@@ -4,4 +4,3 @@ export { useQueuedDebouncedMutation } from './useQueuedDebouncedMutation';
 export { useLocalStorageDraft, type DraftWithTTL } from './useLocalStorageDraft';
 export { useTimer, type UseTimerOptions, type UseTimerReturn } from './use-timer/useTimer';
 export { useZodValidationWithAsyncCheck } from './useZodValidationWithAsyncCheck';
-export { useIsActive } from './useIsActive';
