45cd5b15c071cbad23f7b694807a365e94cde5fa
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2026-06-03 09:51:59 +0300

    refactor(task): remove TaskCard component and clean up exports

--- src/entities/task/index.ts
+++ src/entities/task/index.ts
@@ -1,5 +1,4 @@
 export type * as TTask from './model/types';
 export * as STask from './model/schemas';
 export { TaskHttp } from './api/http';
-export { TaskCard } from './ui/TaskCard';
 export { taskFabricKeys } from './model/const';
