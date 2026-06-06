58e82f9c20fd280fb949ccdd67dde13e7f5c4098
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2026-06-06 17:11:08 +0300

    refactor(useSignup): unificate mutation hook

--- src/pages/auth/signup/model/useSignup.ts
+++ src/pages/auth/signup/model/useSignup.ts
@@ -1,22 +1,17 @@
-import { type DefaultError, useMutation } from '@tanstack/react-query';
+import { type DefaultError, useMutation, UseMutationOptions } from '@tanstack/react-query';
 import { AuthHttp, TAuth } from 'entities/auth';
 
-interface UseSignupProps {
-  onSuccess?: (body: TAuth.SignupBody, res: TAuth.SignupResponse) => void;
-  onError?: (err: Error) => void;
-}
+export type UseSignupOptions = Omit<
+  UseMutationOptions<TAuth.SignupResponse, DefaultError, TAuth.SignupBody>,
+  'mutationFn'
+>;
 
-export function useSignup({ onSuccess, onError }: UseSignupProps = {}) {
+export function useSignup(rest: UseSignupOptions = {}) {
   return useMutation<Awaited<TAuth.SignupResponse>, DefaultError, TAuth.SignupBody>({
     mutationFn: AuthHttp.signup,
     meta: {
       skipGlobalValidationToast: true,
     },
-    onError: (err) => {
-      onError?.(err);
-    },
-    onSuccess: (res, body) => {
-      onSuccess?.(body, res);
-    },
+    ...rest,
   });
 }
