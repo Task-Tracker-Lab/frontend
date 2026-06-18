4fa4ea14c6f64a3b8d3c3ab04257dd5a9b08f14e
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2026-06-19 01:05:17 +0300

    refactor(useClickOutside): replace handler dependency with useRef pattern

--- src/features/task/create/lib/useClickOutside.ts
+++ src/features/task/create/lib/useClickOutside.ts
@@ -1,14 +1,14 @@
-/* eslint-disable check-file/filename-naming-convention */
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 
 export function useClickOutside(id: string, handler: () => void) {
+  const handlerRef = useRef(handler);
+
   useLayoutEffect(() => {
     const listener = (e: MouseEvent) => {
       const elem = document.getElementById(id);
-      if (!elem) return;
-      if (!elem?.contains(e.target as Node)) handler?.();
+      if (!elem?.contains(e.target as Node)) handlerRef.current?.();
     };
     document.addEventListener('mouseup', listener);
     return () => document.removeEventListener('mouseup', listener);
-  }, [handler, id]);
+  }, [id]);
 }
